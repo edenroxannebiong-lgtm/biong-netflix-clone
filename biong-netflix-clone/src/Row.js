a4061c9bfa1b6c081a18e78e1f38739e55e3426f
recroxanneb <edenroxannebiong@gmail.com>
2026-03-28 21:14:56 +0800

    tbc tom

--- biong-netflix-clone/src/Row.js
+++ biong-netflix-clone/src/Row.js
@@ -1,7 +1,10 @@
 import React, {useState, useEffect} from "react";
 import axios from "./axios";
+import "./Row.css";
 
-function Row({ title }) {
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
 
     // A snippet of code which runs based on a specific condition/variable
@@ -9,18 +12,31 @@ function Row({ title }) {
         //run once when the row loads and don't run again
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            //"https://api.themoviedb.org/3"
-
+            
+         //"https://api.themoviedb.org/3
+            console.log(request);
+            setMovies(request.data.results);
+            return request;
 
         }
         fetchData();
-    }, []);
+    }, [fetchUrl]);
 
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-                {/* container -> posters */}
+
+            <div className="row__posters">
+            {/* several row__poster(s) */}  
+
+            {movies.map(movie => (
+                
+            
+                
+                <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+            ))}
+            </div>
         </div>
     );
 }
